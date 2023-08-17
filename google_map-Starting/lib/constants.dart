7fa8cafcccb421c00a23b3435282d45777253e9a
QUYEN <httuquyen01@gmail.com>
2023-08-17 16:23:17 +0700

    thuchanh5

---
+++ google_map-Starting/lib/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyB_aNXc-XyIE5G-AjP7GOmllATImoCmX_M";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
